974d14fc96a6684ace5bf49cea1842d2c33153fa
Nate Gatesh <ngatesh@asu.edu>
2022-08-27 16:54:09 -0700

    Working Code, f(X) = 3.81

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -6,16 +6,20 @@ from scipy.optimize import minimize, LinearConstraint
 
 
 def objective_function(x):
-    return (x[1] - x[2]) ** 2 + (x[2] + x[3] - 2) ** 2 + (x[4] - 1) ** 2 + (x[5] - 1) ** 2
+    return (x[0] - x[1]) ** 2 + (x[1] + x[4] - 2) ** 2 + (x[3] - 1) ** 2 + (x[4] - 1) ** 2
 
 
 A = np.array([[1, 3, 0, 0, 0],
               [0, 0, 1, 1, -2],
               [0, 1, 0, 0, -1]])
 
-B = np.array([0, 0, 0]).T
+B = np.array([0, 0, 0])
 
 constraint = LinearConstraint(A, lb=B, ub=B)
-
 bounds = [(-10, 10) for i in range(5)]
+guess = np.array([0, 0, 0, 0, 0])
+
+res = minimize(objective_function, x0=guess, constraints=constraint, bounds=bounds)
+
+print(res)
 
