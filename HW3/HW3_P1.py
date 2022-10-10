bc39779cf86f0a6a276557e37b1bbd1673b128ef
Nate Gatesh <ngatesh@asu.edu>
2022-10-09 20:11:56 -0700

    Program working

---
+++ HW3/HW3_P1.py
@@ -0,0 +1,46 @@
+import numpy as np
+import torch
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+
+T = 20
+a_w = np.array([8.07131, 1730.63, 233.426])
+a_d = np.array([7.43155, 1554.679, 240.337])
+
+p_w = 10 ** (a_w[0] - a_w[1]/(T + a_w[2]))
+p_d = 10 ** (a_d[0] - a_d[1]/(T + a_d[2]))
+
+P = np.array([[28.1, 34.4, 36.7, 36.9, 36.8, 36.7, 36.5, 35.4, 32.9, 27.7, 17.5]])
+X1 = np.array([[0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]])
+X2 = 1 - X1
+
+P = torch.tensor(P, requires_grad=False, dtype=torch.float32)
+X1 = torch.tensor(X1, requires_grad=False, dtype=torch.float32)
+X2 = torch.tensor(X2, requires_grad=False, dtype=torch.float32)
+A = Variable(torch.tensor([1.0, 1.0]), requires_grad=True)
+
+pred = 0
+loss = 0
+a = 0.0001
+
+thisA = np.array([[0, 0]])
+lastA = np.array([[1, 1]])
+deltaALim = np.ones([1, 2]) * 10**-8
+
+
+for i in range(100):
+    pred = X1 * torch.exp(A[0] * (A[1]*X2 / (A[0]*X1 + A[1]*X2)) ** 2) * p_w \
+         + X2 * torch.exp(A[1] * (A[0]*X1 / (A[0]*X1 + A[1]*X2)) ** 2) * p_d
+
+    loss = ((P - pred) ** 2).sum()
+    loss.backward()
+
+    with torch.no_grad():
+        A -= a * A.grad
+        A.grad.zero_()
+
+        lastA = thisA.copy()
+        thisA = A.data.numpy().copy()
+
+print(f'A12 : {A[0]}\tA21 : {A[1]}\tLoss : {loss.data.numpy()}')
+
