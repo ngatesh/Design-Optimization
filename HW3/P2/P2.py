3bca7cd0b9061855285661b4f320774da82f21c0
Nate Gatesh <ngatesh@asu.edu>
2022-10-11 15:23:24 -0700

    Convergence plot, increased number of calls

--- HW3/P2/P2.py
+++ HW3/P2/P2.py
@@ -1,4 +1,7 @@
+from matplotlib import pyplot as plt
 from skopt import gp_minimize
+from skopt.plots import plot_convergence
+
 
 def f(x):
     x1 = x[0]
@@ -6,6 +9,8 @@ def f(x):
     return (4 - 2.1*x1**2 + 1/3*x1**4)*x1**2 + x1*x2 + (-4 + 4*x2**2)*x2**2
 
 
-res = gp_minimize(f, [(-3.0, 3.0), (-2.0, 2.0)], n_calls=15, n_random_starts=5, random_state=5657)
+res = gp_minimize(f, [(-3.0, 3.0), (-2.0, 2.0)], n_calls=40, n_random_starts=10, random_state=5657)
 
 print(f"X1= {res.x[0]}\nX2= {res.x[1]}\nf= {res.fun}")
+plot_convergence(res)
+plt.show()
