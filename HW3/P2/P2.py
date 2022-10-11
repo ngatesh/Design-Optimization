68bc205c7fed89940b598a1e078c2d050338212a
Nate Gatesh <ngatesh@asu.edu>
2022-10-11 15:18:22 -0700

    Fixed giving integer results

--- HW3/P2/P2.py
+++ HW3/P2/P2.py
@@ -6,6 +6,6 @@ def f(x):
     return (4 - 2.1*x1**2 + 1/3*x1**4)*x1**2 + x1*x2 + (-4 + 4*x2**2)*x2**2
 
 
-res = gp_minimize(f, [(-3, 3), (-2, 2)], n_calls=15, n_random_starts=5, random_state=5657)
+res = gp_minimize(f, [(-3.0, 3.0), (-2.0, 2.0)], n_calls=15, n_random_starts=5, random_state=5657)
 
 print(f"X1= {res.x[0]}\nX2= {res.x[1]}\nf= {res.fun}")
