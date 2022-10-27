ad28bf33bf1804a198a7896f782ef35e03718538
Nate Gatesh <ngatesh@asu.edu>
2022-10-26 19:23:27 -0700

    commented Simulation class

--- Project1/Simulation.py
+++ Project1/Simulation.py
@@ -6,36 +6,66 @@ from numpy.random import uniform
 class Simulation(nn.Module):
 
     def __init__(self, controller, dynamics, T, batchSize):
+        """
+        :param controller: Control system object.
+        :param dynamics: Dynamics definition object.
+        :param T: Number of time steps to simulate.
+        :param batchSize: Number of simulations to optimize simultaneously.
+        """
         super(Simulation, self).__init__()
+
+        # Instance variables.
         self.controller = controller
         self.dynamics = dynamics
         self.T = T
         self.batchSize = batchSize
-        self.state = self.initialize_state()
+
+        # Generate {batchSize} initial states to simulate.
+        self.states = self.initialize_states()
+
+        # Storage for state and action trajectories through all time steps.
         self.action_trajectory = []
         self.state_trajectory = []
 
-    def forward(self, state):
-        loss = 0
+    def forward(self, states):
+        """
+        Run the simulation to completion, given a set of initial states.
+
+        :param states: Initial states.
+        :return: Loss value.
+        """
+        loss = 0    # Loss value placeholder.
+
+        # Run through every initial condition in the batch of initial states.
         for i in range(self.batchSize):
+            thisState = states[i]   # Initial state to simulate.
+
+            # Reset trajectories.
             self.action_trajectory = []
             self.state_trajectory = []
 
-            thisState = state[i]
-
+            # Loop through all time steps.
             for _ in range(self.T):
-                action = self.controller.forward(thisState)
-                thisState = self.dynamics.forward(thisState, action)
+                action = self.controller.forward(thisState)             # Determine action from the controller.
+                thisState = self.dynamics.forward(thisState, action)    # Update system state through dynamics.
+
+                # Append new action and state to the trajectories.
                 self.action_trajectory.append(action)
                 self.state_trajectory.append(thisState)
 
+            # Calculate the error in the final state, then add it to the total loss.
             loss = loss + self.error(thisState)
 
+        # Normalize loss to the batch size.
         return loss / self.batchSize
 
     def initialize_state(self):
+        """
+        :return: an array of initial state tensors, randomly generated.
+        """
         states = []
         for _ in range(self.batchSize):
+            # Generate random states values.
             xState = uniform(-50, 50)
             yState = uniform(50, 110)
             thetaState = uniform(-0.75, 0.75)
@@ -43,20 +73,17 @@ class Simulation(nn.Module):
             yDotState = uniform(-30, 0)
             thetaDotState = uniform(-0.02, 0.02)
 
+            # Combine into a tensor object and add to state array.
             state = t.tensor([xState, yState, thetaState, xDotState, yDotState, thetaDotState], requires_grad=False)
-            #state = t.tensor([1, 1, 0, 0, 0, 0], requires_grad=False).float()
+            # state = t.tensor([1, 1, 0, 0, 0, 0], requires_grad=False).float()
             states.append(state)
 
         return states
 
     @staticmethod
     def error(state):
+        """
+        :param state: System state
+        :return: Weighted error
+        """
         return state[0]**2 + state[1]**2 + 100*state[2]**2 + state[3]**2 + state[4]**2 + 100*state[5]**2
-
-    def error1(self):
-        total = 0
-        for i in range(self.T - 1):
-            total = total + t.sum(self.state_trajectory[i] ** 2)
-        total = total + 1000.0 * t.sum(self.state_trajectory[-1] ** 2)
-
-        return total
