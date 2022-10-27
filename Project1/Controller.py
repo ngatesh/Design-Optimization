fcd694b8dd86343cf0dffbc3a35d4f6808457a7e
Nate Gatesh <ngatesh@asu.edu>
2022-10-26 18:59:54 -0700

    commented controller class

--- Project1/Controller.py
+++ Project1/Controller.py
@@ -1,19 +1,22 @@
 import torch as t
 from torch import nn
 
+# Rocket constants sourced roughly from the Falcon 9.
 MAX_THRUST = 500_000.0  # maximum thrust force [N]
-MAX_PHI = 0.52        # maximum thrust angle [rad]. Approx 30deg.
+MAX_PHI = 0.52          # maximum thrust angle [rad]. Approx 30deg.
 
 
 class Controller(nn.Module):
-
     def __init__(self, dim_input, dim_hidden, dim_output):
         """
-        dim_input: Number of system states
-        dim_output: Number of actions
-        dim_hidden: Width of hidden net
+        :param dim_input: Number of system states.
+        :param dim_hidden: Number of action states.
+        :param dim_output: Width of hidden net.
         """
+
         super(Controller, self).__init__()
+
+        # Define neural net with two hidden layers and hyperbolic-tangent activations.
         self.network = nn.Sequential(
             nn.Linear(dim_input, dim_hidden),
             nn.Tanh(),
@@ -22,9 +25,13 @@ class Controller(nn.Module):
             nn.Linear(dim_hidden, dim_output)
         )
 
+    # Produce the control action given a system state.
     def forward(self, state):
-        action = self.network(state)
+        action = self.network(state)    # Get network result.
+
+        # Activate thrust with sigmoid to constrain from 0 to MAX_THRUST.
         thrust = t.sigmoid(action[0]) * MAX_THRUST
+        # Activate thrust angle with tanh to constrain between +/- MAX_PHI.
         phi = t.tanh(action[1]) * MAX_PHI
 
         return t.stack((thrust, phi), dim=0)
