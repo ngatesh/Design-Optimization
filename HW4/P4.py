8ef64e6dedffb82b27b167df3b6b4e6b8452e809
Nate Gatesh <ngatesh@asu.edu>
2022-11-13 18:10:34 -0700

    added lineSearch and solve functions

--- HW4/P4.py
+++ HW4/P4.py
@@ -1,4 +1,18 @@
 import numpy as np
+from numpy.linalg import inv
+
+k = 0
+epsilon = 10**-3
+
+
+def _h(s, d):
+    h1 = 1/4*s[0]**2 + 1/5*s[1]**2 + 1/25*d[0]**2 - 1
+    h2 = s[0] + s[1] - d[0]
+    return np.array([[h1, h2]]).T
+
+
+def _f(s, d):
+    return s[0]**2 + s[1]**2 + d[0]**2
 
 
 def df_dd(s, d):
@@ -18,11 +32,61 @@ def dh_ds(s, d):
                      [1,        1]])
 
 
-k = 0
-epsilon = 10**-3
+def df_Dd(s, d):
+    dfdd = df_dd(s, d)
+    dfds = df_ds(s, d)
+    dhdd = dh_dd(s, d)
+    dhds_inv = inv(dh_ds(s, d))
 
-s = np.array([1, 2])
-d = np.array([3])
+    return dfdd - np.matmul(np.matmul(dfds, dhds_inv), dhdd)
+
+
+def solve(s, d):
+    h = _h(s, d)
+
+    while h.T.h > epsilon:
+        s = s.T - np.matmul(np.invert(dh_ds(s, d)), h)
+        h = _h(s, d)
+
+    return s
+
+
+def lineSearch(dfDd, s, d):
+    a = 1
+    b = 0.5
+    t = 0.3
+
+    f = 1
+    phi = 0
+
+    while f > phi:
+
+        dhds_inv = inv(dh_ds(s, d))
+        dhdd = dh_dd(s, d)
+        dfdd = df_dd(s, d)
+
+        dd = -a * dfDd
+        ds = a * np.matmul(np.matmul(dhds_inv, dhdd), dfdd.T).T
+
+        f = _f(s + ds, d + dd)
+        phi = _f(s, d) - a * t * np.matmul(dfDd, dfDd.T)
+
+        a = b * a
+
+    return a
+
+
+d = np.array([1])
+s = np.array([1, 1])
+s = solve(s, d)
+
+dfDd = df_Dd(s, d)
+
+while dfDd.T.dfDd > epsilon:
+    alpha = lineSearch(dfDd, s, d)
+    d = d - alpha * dfDd
+    s
+
 
 print(df_dd(s, d))
 print(df_ds(s, d))
